8f2ea9b684bc4da203135be5230c25e39cc0aa7f
a-random441 <somerandomgithubacc@gmail.com>
2024-09-28 18:55:02 +0100

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -11,6 +11,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	
 	#if sys
 	public static var cwd:String;
@@ -28,8 +29,21 @@ class Main extends Sprite
 		#if sys
 		cwd = SUtil.getStorageDirectory();
 		#end
+	private function setupGame():Void
+	{
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
+		{
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.min(ratioX, ratioY);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
+		}
 		SUtil.checkFiles();
-		addChild(new FlxGame(0, 0, TitleState, 1, gameWidth, gameHeight, OptionsHandler.options.fpsCap, OptionsHandler.options.fpsCap, true));
+		addChild(new FlxGame(0, 0, TitleState, 1, gameWidth, gameHeight, zoom, OptionsHandler.options.fpsCap, OptionsHandler.options.fpsCap, true));
 		//#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
 		addChild(new MemoryCounter(10, 3, 0xFFFFFF));
