66256a2f20da4e1b04b7479bc1a16cf852787eb2
a-random441 <somerandomgithubacc@gmail.com>
2024-09-28 18:40:05 +0100

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,6 +9,9 @@ import plugins.ExamplePlugin.ExampleCharPlugin;
 #end
 class Main extends Sprite
 {
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	
 	#if sys
 	public static var cwd:String;
 	#end
@@ -26,7 +29,7 @@ class Main extends Sprite
 		cwd = SUtil.getStorageDirectory();
 		#end
 		SUtil.checkFiles();
-		addChild(new FlxGame(0, 0, TitleState, 1, OptionsHandler.options.fpsCap, OptionsHandler.options.fpsCap, true));
+		addChild(new FlxGame(0, 0, TitleState, 1, gameWidth, gameHeight, OptionsHandler.options.fpsCap, OptionsHandler.options.fpsCap, true));
 		//#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
 		addChild(new MemoryCounter(10, 3, 0xFFFFFF));
